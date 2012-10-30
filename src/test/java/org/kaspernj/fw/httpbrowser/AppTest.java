104c7ebe37a57f6c4c97f10aaa543e91102d5fce
Kasper Johansen <k@spernj.org>
2012-10-30 21:15:22 +0100

    Added missing post multipart test. Added more stuff to the readme.

--- src/test/java/org/kaspernj/fw/httpbrowser/AppTest.java
+++ src/test/java/org/kaspernj/fw/httpbrowser/AppTest.java
@@ -4,7 +4,11 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -184,6 +188,66 @@ public class AppTest
 			System.out.println("Cookies are the same (" + http.getCookies().toString() + ").");
 			System.out.println("Cookie1: " + cookieValue);
 			System.out.println("Cookie2: " + cookieValue2);
+		}
+	}
+	
+	public void testPostMultipart() throws Exception{
+		System.out.println("Spawning object.");
+		HttpBrowser http = new HttpBrowser();
+		http.setHost("partyworm.dk");
+		http.setPort(80);
+		http.setDebug(false);
+		http.setEncodingGZIP(true);
+		
+		System.out.println("Connecting.");
+		http.connect();
+		
+		System.out.println("Generating request.");
+		File curFile = new File(".");
+		
+		
+		
+		
+		HttpBrowserRequestPostMultipart req = http.postMultipart();
+		req.setAddress("multipart_test.php");
+		req.addPost("TestArgument1", "This is a test");
+		
+		System.out.println("Cur path: " + curFile.getCanonicalPath());
+		
+		HttpBrowserRequestPostMultipartFileUpload fu = req.addFileUpload();
+		fu.setPostName("some_file");
+		fu.setFilePath(curFile.getCanonicalPath() + "/src/test/java/org/kaspernj/fw/httpbrowser/TestMultipartPostExampleUploadFile.txt");
+		fu.setContextType("text/plain");
+		
+		HttpBrowserResult res = req.execute();
+		if (!res.getBody().contains("{\"TestArgument1\":\"This is a test\"}")){
+			throw new Exception("Unexpected body: " + res.getBody());
+		}else if(!res.getBody().contains(",\"files\":{\"some_file\":{\"name\":\"TestMultipartPostExampleUploadFile.txt\"")){
+			throw new Exception("Unexpected body: " + res.getBody());
+		}
+		
+		System.out.println("Body: " + res.getBody());
+		
+		
+		http.setDebug(false);
+		
+		HttpBrowserRequestPostMultipart reqJPEG = http.postMultipart();
+		reqJPEG.setAddress("multipart_test.php?choice=file-test");
+		reqJPEG.addPost("TestArgument1", "This is a test");
+		
+		HttpBrowserRequestPostMultipartFileUpload fuJPEG = reqJPEG.addFileUpload();
+		fuJPEG.setPostName("file");
+		fuJPEG.setFilePath(curFile.getCanonicalPath() + "/src/test/java/org/kaspernj/fw/httpbrowser/TestMultipartPostExampleUploadFile.jpeg");
+		fuJPEG.setContextType("text/plain");
+		
+		HttpBrowserResult resJPEG = reqJPEG.execute();
+		byte[] bodyBytes = resJPEG.getBodyAsByteArray();
+		byte[] fileBytes = Files.readAllBytes(Paths.get(curFile.getCanonicalPath() + "/src/test/java/org/kaspernj/fw/httpbrowser/TestMultipartPostExampleUploadFile.jpeg"));
+		
+		if (bodyBytes.length != fileBytes.length){
+			throw new Exception("The two byte-arrays did not have the same length: " + bodyBytes.length + ", " + fileBytes.length + "\n\n'" + (new String(bodyBytes)) + "'\n\n'" + (new String(fileBytes)) + "'.");
+		}else if(!Arrays.equals(bodyBytes, fileBytes)){
+			throw new Exception("Expected body byte array to be exactly the same as the original file byte array but it wasnt.");
 		}
 	}
 }
